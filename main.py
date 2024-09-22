2f706cb90cd82c38588cdb6b734aa16c117d243c
hazhir <lhazhi@yahoo.com>
2024-09-23 00:23:57 +0330

    Fixed multiple issues in the countdown timer functionality.
    - Countdown timer now correctly reflects user-selected time.
    - Improved error handling for invalid key inputs.
    - Resolved bug with extra zero in countdown display.
    - Added try/except blocks for better error management.

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def timer(minute):
     pomodoro = int(minute / 30)
     playsound("soundfiles/Start.wav")
 
-    for _ in range(pomodoro):
-       
-        for mins_left in range(30 - 1, -1, -1):
+    for _ in range(minute):
+
+        for mins_left in range(minute - 1, -1, -1):
             
             for secs_left in range(59, -1, -1):
                 
@@ -17,32 +17,39 @@ def timer(minute):
 
                 if mins_left == 0 and secs_left == 0:
                     playsound("soundfiles/Finish.wav")
-                    print("time's up!")
+                    print("Time's up!")
+                    return
 
-                if secs_left < 10:
-                    secs_left = f"0{secs_left}"
-                    
-                print(f"{mins_left}:{secs_left}",end="\r")
-
+                print(f"{mins_left:02}:{secs_left:02}", end = "\r") 
                 time.sleep(1)
-
-
+                
 print(open("textfiles/intro.txt", 'r').read())
-print(open("textfiles/durnations.txt", 'r').read())
+print(open("textfiles/durations.txt", 'r').read())
 
-user_input = int(input("Choose your Durnation: "))
 
-pomos = {
-    1 : 30,
-    2 : 60, 
-    3 : 90,
-    4 : 120,
-    5 : 150,
-    6 : 180
-    }
 
-if user_input in pomos.keys():
-    print(f"Your {pomos[user_input]} minutes durnaion has begun.")
-    timer(pomos[user_input])
-else:
-    print("You pressed the wrong key buddy!, CHOOSE AGAIN")
+
+while True:
+    
+    try:
+        user_input = int(input("Choose your duration: "))
+
+        pomos = {
+            1 : 30,
+            2 : 60, 
+            3 : 90,
+            4 : 120,
+            5 : 150,
+            6 : 180
+            }
+        
+        if user_input in pomos.keys():
+            print(f"Your {pomos[user_input]} minutes duration has begun.")
+            timer(pomos[user_input])
+        else:
+            print("come on man! it's simple. Choose a number from 1 to 6.")
+    except ValueError:
+        print("WOW! you went for something other than numbers? choose 1 or 2 or 3 or 4 or 5 or 6. Okay?")
+
+
+
